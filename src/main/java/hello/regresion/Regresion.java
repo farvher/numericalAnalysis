1c32a1f1bcfda381c0c76c7f6cb1f66def81be7f
Farith Sanmiguel <fesanmiguel@misena.edu.co>
2020-03-10 17:22:59 -0500

    regresion lineal ejemplo

---
+++ src/main/java/hello/regresion/Regresion.java
@@ -0,0 +1,64 @@
+package hello.regresion;
+
+public class Regresion {
+    private double[] x;
+    private double[] y;
+    private int n;          //número de datos
+    public double a, b;    //pendiente y ordenada en el origen
+
+
+    public static void main(String[] args) {
+        double[] temperatura={5, 7, 10, 12, 16, 20, 23, 27, 19, 14, 9, 6};
+        double[] ventas={9, 11, 15, 16, 20, 24, 27, 29, 22, 20, 14, 9};
+        Regresion regresion=new Regresion(temperatura, ventas);
+        regresion.lineal();
+        System.out.println("Pendiente             "+regresion.a);
+        System.out.println("Ordenada en el origen "+regresion.b);
+
+        System.out.println("Indice de correlación "+regresion.correlacion());
+    }
+
+    public Regresion(double[] x, double[] y) {
+        this.x = x;
+        this.y = y;
+        n = x.length; //número de datos
+    }
+
+    public void lineal() {
+        double pxy, sx, sy, sx2, sy2;
+        pxy = sx = sy = sx2 = sy2 = 0.0;
+        for (int i = 0; i < n; i++) {
+            sx += x[i];
+            sy += y[i];
+            sx2 += x[i] * x[i];
+            sy2 += y[i] * y[i];
+            pxy += x[i] * y[i];
+        }
+        a = (n * pxy - sx * sy) / (n * sx2 - sx * sx);
+        b = (sy - b * sx) / n;
+    }
+
+    public double correlacion() {
+//valores medios
+        double suma = 0.0;
+        for (int i = 0; i < n; i++) {
+            suma += x[i];
+        }
+        double mediaX = suma / n;
+
+        suma = 0.0;
+        for (int i = 0; i < n; i++) {
+            suma += y[i];
+        }
+        double mediaY = suma / n;
+//coeficiente de correlación
+        double pxy, sx2, sy2;
+        pxy = sx2 = sy2 = 0.0;
+        for (int i = 0; i < n; i++) {
+            pxy += (x[i] - mediaX) * (y[i] - mediaY);
+            sx2 += (x[i] - mediaX) * (x[i] - mediaX);
+            sy2 += (y[i] - mediaY) * (y[i] - mediaY);
+        }
+        return pxy / Math.sqrt(sx2 * sy2);
+    }
+}
