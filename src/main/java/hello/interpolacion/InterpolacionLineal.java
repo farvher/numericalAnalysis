d16a743e83c82cdd15f64f7a01a429c0ee1c2963
Farith Sanmiguel <fesanmiguel@misena.edu.co>
2020-03-11 10:13:08 -0500

    regresion lineal

--- src/main/java/hello/interpolacion/InterpolacionLineal.java
+++ src/main/java/hello/interpolacion/InterpolacionLineal.java
@@ -1,5 +1,22 @@
 package hello.interpolacion;
 
+import java.util.function.DoubleFunction;
+
 public class InterpolacionLineal {
+
+    //
+
+    static DoubleFunction<Double> ln = x -> Math.log(x);
+
+    public static void main(String[] args) {
+
+        System.out.printf("interpolacion " +interpolacionLineal(2d, 1d, 6d));
+    }
+
+    public static Double interpolacionLineal(Double x , Double xi, Double xu){
+
+       return  ln.apply(xi) + ((ln.apply(xu) - ln.apply(xi))/(xu-xi)) * (x - xi);
+
+    }
 
 }
