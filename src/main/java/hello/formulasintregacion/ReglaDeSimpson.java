8d4893dfabb9a77b95848af7cf9371d787973eff
Farith Sanmiguel <fesanmiguel@misena.edu.co>
2020-03-12 14:22:11 -0500

    regla del trapecio y simpson

---
+++ src/main/java/hello/formulasintregacion/ReglaDeSimpson.java
@@ -0,0 +1,38 @@
+package hello.formulasintregacion;
+
+import java.util.function.DoubleFunction;
+import java.util.function.ToDoubleBiFunction;
+
+public class ReglaDeSimpson {
+
+
+    static DoubleFunction<Double> fn = x -> 0.2 + 25 * x - 200 * (Math.pow(x, 2)) + 675 * Math.pow(x, 3) - 900 * Math.pow(x, 4) + 400 * Math.pow(x, 5);
+    static ToDoubleBiFunction<Double,Double> simpson = (a,b)  -> (b-a) * ((fn.apply(a) +(4*(fn.apply(b/2))) + fn.apply(b))/6);
+
+    public static void main(String[] args) {
+        System.out.println("REGLA DE SIMPSON f(x) = 0.2 +25x - 200x2 + 675x3 - 900x4 +400x5");
+        System.out.println("valor real de la integral = 1.640533");
+        Double a,b;
+        a = 0d;
+        b = 0.8;
+        System.out.println("resultado simpson I = "+ simpson.applyAsDouble(a,b));
+
+        System.out.println("REGLA DE SIMPSON multiple");
+        Integer n = 4;
+        Double h = (b - a) / n;
+        Double sump = 0d;
+        Double sumi = 0d;
+        Double i = a;
+
+        for (int r = 1; r < n; r++) {
+            Double tmp = fn.apply(i+=h);
+            sump += r%2==0 ? tmp:0d;
+            sumi += r%2!=0 ? tmp:0d;
+        }
+
+
+        Double I = (b-a) *((fn.apply(a) +(4*sumi) + (2*sump) + fn.apply(a))/(n*3));
+        System.out.println("resultado simpson n="+n+" I = "+I);
+    }
+
+}
--- src/main/java/hello/formulasintregacion/ReglaDelTrapecio.java
+++ src/main/java/hello/formulasintregacion/ReglaDelTrapecio.java
@@ -6,17 +6,31 @@ public class ReglaDelTrapecio {
 
     // I = (b - a) (f(a)+f(b)/2)
 
-    static DoubleFunction<Double> fn = x -> 0.2 + 25*x - 200*(Math.pow(x,2)) + 675*Math.pow(x,3) - 900*Math.pow(x,4) + 400*Math.pow(x,5);
-
+    static DoubleFunction<Double> fn = x -> 0.2 + 25 * x - 200 * (Math.pow(x, 2)) + 675 * Math.pow(x, 3) - 900 * Math.pow(x, 4) + 400 * Math.pow(x, 5);
+    static DoubleFunction<Double> fn2 = x -> -400 + 4050*x -10800*Math.pow(x,2) + 8000*Math.pow(x,3);
     public static void main(String[] args) {
-        Double b,a;
+
+        System.out.println("REGLA DEL TRAPECIO f(x) = 0.2 +25x - 200x2 + 675x3 - 900x4 +400x5");
+        System.out.println("valor real de la integral = 1.640533");
+        Double b, a;
         b = 0.8d;
         a = 0d;
         System.out.println(fn.apply(a));
         System.out.println(fn.apply(b));
 
-        Double res = (b-a) * ((fn.apply(a) + fn.apply(b))/2);
-        System.out.println(res);
+        Double res = (b - a) * ((fn.apply(a) + fn.apply(b)) / 2);
+        System.out.println("un solo segmento " + res);
+        Integer n = 4;
+        Double h = (b - a) / n;
+        Double psx = 0d;
+        Double x = a;
+        for (int i = 1; i < n; i++) {
+            psx +=  fn.apply((x+=h));
+        }
+
+        Double I = (b - a) * ((fn.apply(a) + (2*psx) + fn.apply(b)) / (2 * n));
+        System.out.println("segmentos n = " + n +" es " + I);
+
 
     }
 
